2f374beb0869b764ed9bc659a348bd7b39a08e77
Standa Chvila <schvila@gmail.com>
2022-06-18 14:58:03 +0200

    Composition versus Inheritance

---
+++ CompositionVsInheritance.js
@@ -0,0 +1,77 @@
+class Monster {
+  constructor(name) {
+    this.name = name;
+  }
+  attack() {
+    console.log(`${this.name} attacked.`);
+  }
+  walk() {
+    console.log(`${this.name} walked.`);
+  }
+}
+class FlyingMonster extends Monster {
+  fly() {
+    console.log(`${this.name} flew.`);
+  }
+}
+
+class SwimmingMonster extends Monster {
+  swim() {
+    console.log(`${this.name} swam.`);
+  }
+}
+const bear = new Monster('bear');
+bear.attack() //?
+bear.walk() //?
+
+const orel = new FlyingMonster('orel');
+orel.attack() //?
+orel.walk() //?
+orel.fly(); //?
+
+// composition
+function swimmer({name}){
+  return {
+    swim:() => {console.log(`${name} swam.`);}
+  }
+}
+function swimmingMonsterCreator(val){
+  const monster = {name: val}
+  return {
+    ...monster,
+    ...swimmer(monster)
+  }
+}
+function flyer({name}){
+  return {
+    fly:() => {console.log(`${name} flew.`);}
+  }
+}
+function attackerAndWalker({name}){
+  return {
+    attack:() => console.log(`${name} attacked.`),
+    walk:() => console.log(`${name} walked.`)
+  }
+}
+
+
+function swimmingFlyingMonsterCreator(val){
+  const monster = {name: val}
+  return {
+    ...monster,
+    ...swimmer(monster),
+    ...flyer(monster),
+    ...attackerAndWalker(monster)
+  }
+}
+
+const shark = swimmer({name: 'shark'});
+shark.swim(); 
+
+const obj  = swimmingMonsterCreator('tuna');
+obj.swim(); 
+
+const sfm = swimmingFlyingMonsterCreator('monstrum');
+sfm.fly();
+sfm.attack();
+sfm.walk();
--- algFibonacci01.js
+++ algFibonacci01.js
@@ -1,45 +1,47 @@
 function fib1(n) {
-  if (n <= 2) {
-    return 1;
+  if(n <= 2) {
+    return 1
   } else {
-    return fib1(n - 1) + fib1(n - 2);
+    return fib1(n -1) + fib1(n - 2);
   }
 }
-const nr = 20;
+
+
 //fib1(5) //?
-console.log(fib1(nr));
+fib1(8) //?
 //fib1(500) //?
 //fib1(51) //?
 
 function fib(n, prevValues = []) {
-  if (prevValues[n] != null) {
-    prevValues; //?
-    return prevValues[n];
+  if(prevValues[n] != null) { 
+    prevValues //?
+    return prevValues[n]
   }
-  let result;
-  if (n <= 2) {
-    result = 1;
+  let result
+  if(n <= 2) {
+    result =  1
   } else {
-    result = fib(n - 1, prevValues) + fib(n - 2, prevValues);
+    result =  fib1(n -1, prevValues) + fib1(n - 2, prevValues); //?
   }
-  result;
-  prevValues[n] = result;
-  prevValues; //?
+  prevValues[n] = result
   return result;
 }
 
 //fib(8) //?
-console.log(fib(nr)); //?
+fib(8) //?
 //fib(500) //?
 //fib(51) //?
 
 function fib3(n) {
-  const numbers = [1, 1];
-  for (let i = 2; i < n + 1; i++) {
-    numbers.push(numbers[i - 2] + numbers[i - 1]);
+  const numbers = [1,1];
+  let sum = 0
+  for(let i = 2; i < n + 1; i++) {
+    numbers.push(numbers[i - 2] + numbers[i - 1])
+    sum += numbers[i - 2] + numbers[i - 1] 
   }
-  numbers; //?
-  return numbers[n];
+  numbers //?
+  sum
+  return numbers[n]
 }
 
-fib3(8); //?
+fib3(4) //?
