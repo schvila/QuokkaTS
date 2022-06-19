f1602c3258252b6aea2c8b4c6259f00c04fa1056
Standa Chvila <schvila@gmail.com>
2022-06-19 07:31:17 +0200

    for..of and for..in loops

---
+++ forLoops.ts
@@ -0,0 +1,9 @@
+let list = [4, 5, 6];
+// for..in returns a list of keys on the object being iterated
+for (let i in list) {
+  console.log(i);
+}
+// for..of returns a list of values
+for (let i of list) {
+  console.log(i); 
+}
